bca4dcc83db72b8bfa5bb5eff5e7b1e3b1fac6f3
Mexty <m.harchenko@i.ua>
2025-03-01 19:59:54 +0200

    Connect MongoDB

--- lesson-4/01-mongoose/src/app.js
+++ lesson-4/01-mongoose/src/app.js
@@ -1,15 +1,27 @@
 import express from 'express';
 
+import Student from './models/student.js';
+
 const app = express();
 
-app.get('/students', (req, res) => {
-  res.send('Students');
+app.get('/students', async (req, res) => {
+  const students = await Student.find();
+
+  res.json(students);
 });
 
-app.get('/students/:id', (req, res) => {
+app.get('/students/:id', async (req, res) => {
   //   console.log(req.params);
   const { id } = req.params;
-  res.send(`Student ${id}`);
+  const student = await Student.findById(id);
+
+  if (student === null) {
+    res.status(404).send('Student not found');
+  }
+
+  res.json(student);
 });
 
 export default app;
+
+// mongodb+srv://mharchenko:VflFhYzuslkohH2w@cluster0.p6aqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- lesson-4/01-mongoose/src/server.js
+++ lesson-4/01-mongoose/src/server.js
@@ -1,11 +1,19 @@
+import 'dotenv/config';
 import app from './app.js';
+
+import { initDataBaseConnection } from './db.js';
 
 const PORT = process.env.PORT || 9090;
 
-function bootstrap() {
-  app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-  });
+async function bootstrap() {
+  try {
+    await initDataBaseConnection();
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 bootstrap();
